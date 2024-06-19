332584c1f8c5225ca5c77887513ce06a0f0c4de7
fgk789 <haleo7894@naver.com>
2024-06-19 17:31:00 +0900

    Add files via upload

---
+++ hello/src/HelloJava.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(7*8);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
